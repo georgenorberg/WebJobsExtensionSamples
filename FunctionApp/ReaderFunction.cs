06f5c7a7ad28be7c7d3ecd480d9bea7d9573bfe6
Donna Malayeri <lindydonna@gmail.com>
2017-08-15 13:17:43 -0700

    Reorgnize project and update to the latest Function App bits

--- FunctionApp/ReaderFunction.cs
+++ FunctionApp/ReaderFunction.cs
@@ -6,23 +6,61 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
 using SampleExtension;
+using System.IO;
 
 namespace FunctionApp
 {
     public static class ReaderFunction
     {
         [FunctionName("ReaderFunction")]
-        public static HttpResponseMessage Run(
-            HttpRequestMessage req,
-            [HttpTrigger] SampleItem item,
+        [NoAutomaticTrigger]
+        public static void Run(
+            SampleItem item,
             [Sample(FileName = "{Name}")] string contents, // Bind to SampleExtension  
-            TraceWriter log
-         )
+            TraceWriter log)
         {
             log.Info("C# HTTP trigger function processed a request.");
             log.Info("Contents:" + contents);
 
-            return req.CreateResponse(HttpStatusCode.OK, "Contents: " + contents);
+            //return req.CreateResponse(HttpStatusCode.OK, "Contents: " + contents);
         }
+
+        // Bind to input as string
+        // BindToInput<SampleItem> --> Converter --> string
+        [NoAutomaticTrigger]
+        public static void Reader(
+            string name,  // from trigger
+            [Sample(FileName = "{name}")] string contents,
+            TraceWriter log)
+        {
+            log.Info(contents);
+        }
+
+        // Bind to input as rich type:
+        // BindToInput<SampleItem> --> item
+        [NoAutomaticTrigger]
+        public static void Reader2(
+            string name,  // from trigger
+            [Sample(FileName = "{name}")] SampleItem item,
+            TextWriter log)
+        {
+            log.WriteLine($"{item.Name}:{item.Contents}");
+        }
+
+#if false
+        #region Using 2nd extensions
+
+        // Bind to input as rich type:
+        // BindToInput<SampleItem> --> item
+        [NoAutomaticTrigger]
+        public static void Reader3(
+            string name,  // from trigger
+            [Sample(Name = "{name}")] CustomType<int> item,
+            TextWriter log)
+        {
+            log.WriteLine($"Via custom type {item.Name}:{item.Value}");
+        }
+        #endregion
+#endif
     }
 }
